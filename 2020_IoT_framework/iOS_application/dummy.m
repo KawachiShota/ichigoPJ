7611aa8e5e56943424db90408d26d53c9999d9c2
KawachiShota <4seasons.s.s.f.w@gmail.com>
2021-09-15 12:34:28 +0900

    delete

--- 2020_IoT_framework/iOS_application/dummy.m
+++
@@ -1,9 +0,0 @@
-//
-//  dummy.m
-//  iot_framework_ichigo
-//
-//  Created by Kawachi Shota on 2020/06/07.
-//  Copyright © 2020 Kawachi Shota. All rights reserved.
-//
-
-#import <Foundation/Foundation.h>
